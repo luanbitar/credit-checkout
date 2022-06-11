29c3f3a3d34870886a6f7bcb70b48b3a85e6e953
Luan Bitar <7@lbitar.com>
2022-06-11 12:52:08 -0300

    chore: removing unused files

--- src/components/Test.tsx
+++
@@ -1,9 +0,0 @@
-function Test(): JSX.Element {
-  return (
-    <>
-      <h1 className="dassda">dsjahkdjaskldjaskldjaksl</h1>
-    </>
-  );
-}
-
-export default Test;
--- src/components/__tests__/Test.spec.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from "@testing-library/react";
-import Test from "components/Test";
-
-it("renders homepage unchanged", () => {
-  const { container } = render(<Test />);
-  expect(container).toMatchSnapshot();
-});
